1c1e4fd5229b7a05aa71d4e7228360aebd65e7c3
Qiwei Yang <yangqiwei97@gmail.com>
2019-12-08 22:15:09 +1100

    App Bar

--- src/Note.js
+++ src/Note.js
@@ -9,7 +9,6 @@ import Editor from './Editor';
 import CodeBlock from './CodeBlock';
 // import Editor from 'react-simple-code-editor';
 // import hljs from 'highlight.js/lib/highlight';
-// import 'highlight.js/styles/github.css';
 // import markdown from 'highlight.js/lib/languages/markdown';
 // import java from 'highlight.js/lib/languages/java';
 // import python from 'highlight.js/lib/languages/python';
@@ -17,11 +16,89 @@ import CodeBlock from './CodeBlock';
 // hljs.registerLanguage('java', java);
 // hljs.registerLanguage('python', python);
 import CloseIcon from '@material-ui/icons/Close';
-import MinimizeIcon from '@material-ui/icons/Minimize';
+// import MinimizeIcon from '@material-ui/icons/Minimize';
 import SettingsIcon from '@material-ui/icons/Settings';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 
+const themes = [
+  '3024-day',
+  '3024-night',
+  'abcdef',
+  'ambiance',
+  'base16-dark',
+  'bespin',
+  'base16-light',
+  'blackboard',
+  'cobalt',
+  'colorforth',
+  'dracula',
+  'duotone-dark',
+  'duotone-light',
+  'eclipse',
+  'elegant',
+  'erlang-dark',
+  'gruvbox-dark',
+  'hopscotch',
+  'icecoder',
+  'isotope',
+  'lesser-dark',
+  'liquibyte',
+  'lucario',
+  'material',
+  'material-darker',
+  'material-palenight',
+  'material-ocean',
+  'mbo',
+  'mdn-like',
+  'midnight',
+  'monokai',
+  'moxer',
+  'neat',
+  'neo',
+  'night',
+  'nord',
+  'oceanic-next',
+  'panda-syntax',
+  'paraiso-dark',
+  'paraiso-light',
+  'pastel-on-dark',
+  'railscasts',
+  'rubyblue',
+  'seti',
+  'shadowfox',
+  'solarized',
+  'the-matrix',
+  'tomorrow-night-bright',
+  'tomorrow-night-eighties',
+  'ttcn',
+  'twilight',
+  'vibrant-ink',
+  'xq-dark',
+  'xq-light',
+  'yeti',
+  'idea',
+  'darcula',
+  'yonce',
+  'zenburn',
+];
+const ITEM_HEIGHT = 20;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 10 + ITEM_PADDING_TOP,
+      width:150,
+    },
+  },
+};
 
 export default class Note extends React.Component {
   constructor(props) {
@@ -32,6 +109,12 @@ export default class Note extends React.Component {
       // noteDim: {
       //   x: 250, y: 250
       // },
+      id: props.id,
+      // selected: true,
+      openSetting: false,
+      anchorEl: null,
+      anchorElHelp: null,
+      theme: 'monokai',
       mode: 0, // 0 for editting, 1 for display
       markdownSrc: `# Live demo
 
@@ -76,12 +159,9 @@ Pretty neat, eh?
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleMarkdownChange(evt) {
-    this.setState({markdownSrc: evt.target.value})
-    // this.setState({markdownSrc: evt});
-  }
-
   handleClick(e) {
+    // console.log(e.target)
+    if (e.target.className==="handle"||e.target.id==="settingButton"||e.target.id==="helpButton") return;
     if (this.node.contains(e.target)) {
       // clicked inside of the component
       if (window.getSelection().toString()!=="") return;
@@ -93,40 +173,138 @@ Pretty neat, eh?
     // clicked outside
     this.setState({mode: 1});
   }
-  
   componentDidMount() {
     document.addEventListener('click', this.handleClick, false);
+    this.setState({
+      id: this.props.id
+    })
   }
-  
   componentWillUnmount() {
     document.removeEventListener('click', this.handleClick, false);
   }
-
-  // onResize = (e, direction, ref, d) => {
-  //   const {x, y} = this.state.noteDim;
-  //   // console.log(d)
-  //   this.setState({
-  //     noteDim: {
-  //       x: x + d.width,
-  //       y: y + d.height,
-  //     }
-  //   });
-  // }
+  handleMarkdownChange(evt) {
+    this.setState({markdownSrc: evt.target.value})
+    // this.setState({markdownSrc: evt});
+  }
+  handleDelete = () => {
+    console.log("delete" + this.state.id)
+    this.props.callback(this.state.id);
+  }
+  handleSettingClose = () => {
+    this.setState({
+      anchorEl: null
+    });
+  };
+  handleSettingClick = (e) => {
+    this.setState({
+      anchorEl: e.currentTarget,
+    });
+  };
+  handleHelpClose = () => {
+    this.setState({
+      anchorElHelp: null
+    });
+  };
+  handleHelpClick = (e) => {
+    this.setState({
+      anchorElHelp: e.currentTarget
+    });
+  };
 
   render() {
     return (
       <div className="note-root">
-      <Draggable handle=".handle">
+        <Draggable bounds="body" handle=".handle">
           <Resizable 
             defaultSize={{
               width: 200,
               height: 250,
             }}
-            minWidth={200}
-            minHeight={200}
+            // minWidth={200}
+            // minHeight={200}
             // onResize={this.onResize}
           >
             <Paper elevation={10} className="markdown-sticky-note-paper">
+              <div className="handle">
+                <IconButton onClick={this.handleDelete} size="small">
+                  <CloseIcon fontSize="small"/>
+                </IconButton>
+                {/* <IconButton size="small" onClick={this.handleMinimize}>
+                  <MinimizeIcon fontSize="small"/>
+                </IconButton> */}
+                <IconButton 
+                  aria-describedby={Boolean(this.state.anchorEl) ? 'setting-popover' : undefined} 
+                  size="small"
+                  onClick={this.handleSettingClick}
+                >
+                  <SettingsIcon id="settingButton" fontSize="small"/>
+                </IconButton>
+                <Popover
+                  id="setting-popover"
+                  open={Boolean(this.state.anchorEl)}
+                  anchorEl={this.state.anchorEl}
+                  onClose={this.handleSettingClose}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                >
+                  <FormControl style={{zIndex:1, margin:2}}>
+                    <InputLabel id="mutiple-name-label">Editor Theme</InputLabel>
+                    <Select
+                      labelId="mutiple-name-label"
+                      id="mutiple-name"
+                      value={this.state.theme}
+                      onChange={(e) => {this.setState({theme:e.target.value})}}
+                      input={<Input />}
+                      MenuProps={MenuProps}
+                    >
+                      {themes.map(name => (
+                        <MenuItem key={name} value={name} >
+                          {name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Popover>
+                <IconButton 
+                  aria-describedby={Boolean(this.state.anchorElHelp) ? 'help-popover' : undefined} 
+                  size="small"
+                  onClick={this.handleHelpClick}
+                >
+                  <HelpOutlineIcon id="helpButton" fontSize="small"/>
+                </IconButton>
+                <Popover
+                  id="help-popover"
+                  open={Boolean(this.state.anchorElHelp)}
+                  anchorEl={this.state.anchorElHelp}
+                  onClose={this.handleHelpClose}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                >
+                  <div>
+                    <MenuItem key={1} component="a" href="https://github.github.com/gfm/" target="_blank">
+                      How to use markdown? <OpenInNewIcon fontSize='small'/>
+                    </MenuItem>
+                    <MenuItem key={2} component="a" href={this.props.optionsPage} target="_blank" >
+                      Options page <OpenInNewIcon fontSize='small'/>
+                    </MenuItem>
+                  </div>
+                </Popover>
+              </div>
+              
+              {/* <Collapse in={this.state.selected} className="collapse"> */}
+                {/* <Paper> */}
               {/* <InputBase 
                 style={{fontFamily: this.state.fontFamily, fontSize: this.state.fontSize, ...styleText}}
                 id="textfield" 
@@ -137,27 +315,13 @@ Pretty neat, eh?
                 rows={((this.state.noteDim.y-18)/(this.state.fontSize*1.18))}
                 inputProps={{ 'aria-label': 'naked' }}
               /> */}
-              <div className="handle">
-                <IconButton size="small">
-                  <CloseIcon fontSize="small"/>
-                </IconButton>
-                <IconButton size="small">
-                  <MinimizeIcon fontSize="small"/>
-                </IconButton>
-                <IconButton size="small">
-                  <SettingsIcon fontSize="small"/>
-                </IconButton>
-                <IconButton size="small">
-                  <HelpOutlineIcon fontSize="small"/>
-                </IconButton>
-              </div>
               <div ref={node => this.node = node} className="note-pane">
                 { this.state.mode === 0 ?
                   <Editor 
                     value={this.state.markdownSrc} 
-                    theme='monokai'                         // let user choose
-                    fontSize={14}                           // let user choose
-                    fontFamily="Consolas, monaco, monospace"// let user choose
+                    theme={this.state.theme}
+                    fontSize={14}                           // later, let user choose
+                    fontFamily="Consolas, monaco, monospace"// later, let user choose
                     onChange={this.handleMarkdownChange} 
                   />
                   // <div className="editor-pane">
@@ -181,9 +345,11 @@ Pretty neat, eh?
                   />
                 }
               </div>
+              {/* </Paper> */}
+            {/* </Collapse> */}
             </Paper>
           </Resizable>
-      </Draggable>
+        </Draggable>
       </div>
       // </div>
     )
--- src/content.js
+++ src/content.js
@@ -5,17 +5,51 @@ import ReactDOM from 'react-dom';
 import "./content.css";
 import Note from './Note';
  
+
+/** initialise parent div */
 const app = document.createElement('div');
 app.className = "markdown-sticky-note";
 document.body.appendChild(app);
+window.localStorage.setItem('md-curMaxIndex', 1000);
 
+
+/** Used to generate unique IDs. */
+let idCounter = 0; // later will need to record this var in storage or DB...
+const uniqueId= () => {
+  let id = ++idCounter;
+  return id.toString();
+}
+
+
+/** Generate new note when click on extension icon */
+const optionsUrl = chrome.extension.getURL('options.html')
 chrome.runtime.onMessage.addListener(
    function(request, sender, sendResponse) {
       if( request.message === "clicked_extension_action" ) {
-        // create a note when user clicked extension icon
         let div = document.createElement('div');
-        ReactDOM.render(<Note />, div);
+        div.id = uniqueId();
+        div.addEventListener('click', (e) => {
+          let curMaxZIndex = window.localStorage.getItem("md-curMaxIndex");
+          let elArray = document.getElementsByClassName('react-draggable');
+          // console.log('here',elArray);
+          for (let el of elArray) {
+            if (el.contains(e.target)) {
+              el.style.zIndex = curMaxZIndex++;
+            }
+          }
+          window.localStorage.setItem('md-curMaxIndex', curMaxZIndex);
+        });
+        // choose a position
+        // div.style.position = '';
         app.appendChild(div);
+        ReactDOM.render(<Note id={div.id} optionsPage={optionsUrl} callback={deleteNote} />, div);
       }
    }
 );
+
+
+/** remove note frrom DOM */
+const deleteNote = (id) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(id));
+  window.localStorage.removeItem('md-curMaxIndex');
+}
