6d398d5e55b804dbaa832880fb75aa9be66d0888
Qiwei Yang <qiwei.yang@sap.com>
2021-10-26 17:30:23 +0800

    upgrade packages

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -338,7 +338,7 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     // new BundleAnalyzerPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
