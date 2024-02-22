87998fece674711a94cb005a87399901a394b6b6
Qiwei Yang <yangqiwei97@gmail.com>
2024-02-22 21:14:21 +0800

    fix: theme

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -58,24 +58,24 @@ export default defineBackground({
         });
         browser.runtime.openOptionsPage();
         // FIXME: comment out this note after testing
-        // browser.storage.local
-        //   .set({
-        //     "https://www.google.com/": [
-        //       {
-        //         content: "",
-        //         font: '"Consolas", "monaco", monospace',
-        //         height: 250,
-        //         id: "3",
-        //         opacity: 1,
-        //         theme: "monokai",
-        //         width: 200,
-        //         fontSize: 14,
-        //         x: 399,
-        //         y: 178,
-        //       },
-        //     ],
-        //   })
-        //   .then(() => {});
+        browser.storage.local
+          .set({
+            "https://www.google.com/": [
+              {
+                content: "",
+                font: '"Consolas", "monaco", monospace',
+                height: 250,
+                id: "3",
+                opacity: 1,
+                theme: "3024-day",
+                width: 200,
+                fontSize: 14,
+                x: 399,
+                y: 178,
+              },
+            ],
+          })
+          .then(() => {});
       }
       // if (details.OnInstalledReason === "update") {
       //    // on extension update
